4ece121b9e2bcf517126f8e6b7b2151ceb117848
James G <jamgyamfi@hotmail.com>
2018-04-12 09:29:17 +0100

    Added Jest and Enzyme

---
+++ src/ShoppingListWeb/__test__/ShoppingListElementTests.tsx
@@ -0,0 +1,47 @@
+﻿import * as React from "react";
+import { HTMLAttributes, shallow, ShallowWrapper } from "enzyme";
+import ShoppingListElement from "../ClientApp/components/common/ShoppingListElement";
+import { ShoppingListElementState, ShoppingListItem } from "../ClientApp/store/ShoppingListElement";
+
+import * as enzyme from 'enzyme';
+import * as Adapter  from 'enzyme-adapter-react-15';
+
+enzyme.configure({ adapter: new Adapter() });
+
+const testShoppingListElementProps: ShoppingListElementState = {
+
+    item: {
+        id: 1,
+        item: "Test",
+        quantity: 2,
+        category: { id: 1, name: "TestCategory" }
+
+    },
+    showDelete: (item: ShoppingListItem): void => { }
+
+
+};
+
+let child: ShallowWrapper<undefined, undefined>;
+
+
+
+beforeEach(() =>
+    child = shallow(<ShoppingListElement {...testShoppingListElementProps} />));
+
+ //checking that all is fine and component has been rendered
+it("should render without error", () => {
+    console.log(child);
+    expect(child.length).toBe(1));
+};
+
+
+
+test('adds 1 + 2 to equal 3', () => {
+
+    let temp: number = 1 + 2;
+
+    expect(temp).toBe(3);
+});
+
+    
